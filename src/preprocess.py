f0a116b410a14fd303fa2414a4cccbff1f129421
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-02-24 11:42:47 -0500

    second

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,5 +5,5 @@ import json
 f = open('/home/tup51337/dataset/Natural-Instructions/subtask061_ropes_answer_generation.json')
 data = json.load(f)
 print(len(data))
-print(data)
+print(data["Contributors"])
 f.close()
