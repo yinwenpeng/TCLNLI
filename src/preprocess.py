f2505b71295becd278447dc30ab9c6873e5992d9
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-03-06 09:11:21 -0500

    preprocess for bart sequential finetune

--- src/preprocess.py
+++ src/preprocess.py
@@ -27,8 +27,10 @@ def MNLI_2_csvformat(filename):
     csvfile.close()
     print('write over')
 
-
-def load_a_single_json_file(fil):
+def load_instruction_from_json_data(fil):
+    '''
+    return: instruction_text_part, instruction_pos_example_part, pos_examples_as_X&Y_format
+    '''
     # f = codecs.open(path+fil, 'r', 'utf-8')
     f = open(fil)
     data = json.load(f)
@@ -44,19 +46,29 @@ def load_a_single_json_file(fil):
     caution_str = data["Emphasis & Caution"].strip()
     # print('caution_str:', caution_str)
     INSTRUCTION = '[Title] '+title_str+' [Prompt] '+prompt_str+' [Definition] '+def_str+' [Avoid] '+avoid_str+' [Caution] '+caution_str
+    INSTRUCTION_text_part = INSTRUCTION.strip().replace('\n', ' ')
 
+
+    pos_ex_tuple_list = []
     POS = ''
     for id, ex in enumerate(data["Examples"]["Positive Examples"]):
         POS+='[POS'+str(id+1)+'] '
+        pos_ex_tuple_list.append((ex['input'].replace('Question:', '[Question]').replace('\n', ' ').strip(), ex['output'].strip()))
         for key, value in ex.items():
             POS+='['+key.strip()+'] '+value.strip()+' '
-    INSTRUCTION+=POS
 
-    # for ex in enumerate(data["Examples"]["Negative Examples"]):
-    #     for key, value in ex.items():
-    #         print(key)
+    INSTRUCTION_pos_example_part = POS.strip().replace('\n', ' ')
 
-    # print('INSTRUCTION:', INSTRUCTION)
+    f.close()
+    return INSTRUCTION_text_part, INSTRUCTION_pos_example_part, pos_ex_tuple_list
+
+def load_a_single_json_file(fil):
+    # f = codecs.open(path+fil, 'r', 'utf-8')
+    f = open(fil)
+    data = json.load(f)
+
+    INSTRUCTION_text, POS_ex, _ = load_instruction_from_json_data(fil)
+
     '''instances'''
     returned_tuple_list = []
     for id, instance in enumerate(data["Instances"]):
@@ -70,7 +82,8 @@ def load_a_single_json_file(fil):
             else:
                 output = ' '.join(value).strip()
         '''put input at the beginning of the long instruction text'''
-        X = input.replace('\n', ' ').replace('Question:', '[Question]').strip()+' '+INSTRUCTION.replace('\n', ' ')
+        input = input.replace('\n', ' ').replace('Question:', '[Question]').strip()
+        X = input+' '+INSTRUCTION_text+' '+POS_ex
         Y = output
         # print('X: ', X)
         # print('Y: ', Y)
@@ -129,6 +142,29 @@ def merge_test_tasks_into_one_category(path, filelist, output_file):
     print('merge over')
 
 
+def generate_training_examples_from_instruction(input_folder, output_folder):
+    file_set = set(os.listdir(input_folder))
+    for fil in file_set:
+        prefix = fil[:fil.find('.json')]
+        csvfile_name = prefix+'.csv'
+        write_csv_filename = output_folder+'/'+csvfile_name
+        csvfile = codecs.open(write_csv_filename, 'w', 'utf-8')
+        fieldnames = ['input', 'output']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+
+        # _, _, POS_ex_tuple_list = load_instruction_from_json_data(input_folder+'/'+fil)
+        INSTRUCTION_text, POS_ex, POS_ex_tuple_list = load_instruction_from_json_data(input_folder+'/'+fil)
+        # training_examples = []
+        for tuple in POS_ex_tuple_list:
+            X = tuple[0]+' '+INSTRUCTION_text+' '+POS_ex
+            Y = tuple[1]
+            writer.writerow({'input': X.strip(), 'output': Y.strip()})
+        csvfile.close()
+        print(fil, ' convert over...')
+    print('Done!')
+
 
 
 
@@ -137,10 +173,13 @@ if __name__ == '__main__':
     # MNLI_2_csvformat('/home/tup51337/dataset/MNLI/multinli_1.0_dev_mismatched.jsonl')
     # convert_data_into_csv('/home/tup51337/dataset/Natural-Instructions/train_original_paper', '/home/tup51337/dataset/Natural-Instructions/train_tasks_csv', '/home/tup51337/dataset/Natural-Instructions/all_training_tasks_in_single_csv.csv')
     # convert_data_into_csv('/home/tup51337/dataset/Natural-Instructions/test_original_paper', '/home/tup51337/dataset/Natural-Instructions/test_tasks_csv', None)
-    test_csv_path = '/home/tup51337/dataset/Natural-Instructions/test_tasks_csv/'
-    merge_test_tasks_into_one_category(test_csv_path, ['subtask002_quoref_answer_generation.csv', 'subtask033_winogrande_answer_generation.csv'], 'AG.csv')
-    merge_test_tasks_into_one_category(test_csv_path, ['subtask003_mctaco_question_generation_event_duration.csv', 'subtask040_qasc_question_generation.csv'], 'QG.csv')
-    merge_test_tasks_into_one_category(test_csv_path, ['subtask005_mctaco_wrong_answer_generation_event_duration.csv', 'subtask008_mctaco_wrong_answer_generation_transient_stationary.csv'], 'IAG.csv')
-    merge_test_tasks_into_one_category(test_csv_path, ['subtask022_cosmosqa_passage_inappropriate_binary.csv', 'subtask052_multirc_identify_bad_question.csv'], 'CF.csv')
-    merge_test_tasks_into_one_category(test_csv_path, ['subtask034_winogrande_question_modification_object.csv', 'subtask045_miscellaneous_sentence_paraphrasing.csv'], 'MM.csv')
-    merge_test_tasks_into_one_category(test_csv_path, ['subtask039_qasc_find_overlapping_words.csv', 'subtask044_essential_terms_identifying_essential_words.csv'], 'VF.csv')
+
+    # test_csv_path = '/home/tup51337/dataset/Natural-Instructions/test_tasks_csv/'
+    # merge_test_tasks_into_one_category(test_csv_path, ['subtask002_quoref_answer_generation.csv', 'subtask033_winogrande_answer_generation.csv'], 'AG.csv')
+    # merge_test_tasks_into_one_category(test_csv_path, ['subtask003_mctaco_question_generation_event_duration.csv', 'subtask040_qasc_question_generation.csv'], 'QG.csv')
+    # merge_test_tasks_into_one_category(test_csv_path, ['subtask005_mctaco_wrong_answer_generation_event_duration.csv', 'subtask008_mctaco_wrong_answer_generation_transient_stationary.csv'], 'IAG.csv')
+    # merge_test_tasks_into_one_category(test_csv_path, ['subtask022_cosmosqa_passage_inappropriate_binary.csv', 'subtask052_multirc_identify_bad_question.csv'], 'CF.csv')
+    # merge_test_tasks_into_one_category(test_csv_path, ['subtask034_winogrande_question_modification_object.csv', 'subtask045_miscellaneous_sentence_paraphrasing.csv'], 'MM.csv')
+    # merge_test_tasks_into_one_category(test_csv_path, ['subtask039_qasc_find_overlapping_words.csv', 'subtask044_essential_terms_identifying_essential_words.csv'], 'VF.csv')
+
+    generate_training_examples_from_instruction('/home/tup51337/dataset/Natural-Instructions/test_original_paper', '/home/tup51337/dataset/Natural-Instructions/test_tasks_instruction_into_examples_csv', None)
