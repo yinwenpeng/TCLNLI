ab681259efb7e9e70c0a865f94801f2e2b9e29d5
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-03-08 09:55:17 -0500

    coding

--- src/baseline_BART.py
+++ src/baseline_BART.py
@@ -430,14 +430,17 @@ def main():
         inputs = [prefix + inp for inp in inputs]
         #model_inputs: dict_keys(['input_ids', 'attention_mask'])
         for input in inputs:
-            print('input:', input)
+            # print('input:', input)
             model_inputs = tokenizer(input, max_length=args.max_source_length, padding=padding, truncation=True)
-        print('tokenize inputs over')
-        exit(0)
+        # print('tokenize inputs over')
+        # exit(0)
         # Setup the tokenizer for targets
         with tokenizer.as_target_tokenizer():
-            labels = tokenizer(targets, max_length=max_target_length, padding=padding, truncation=True)
-
+            for target in targets:
+                print('target:', target)
+                labels = tokenizer(target, max_length=max_target_length, padding=padding, truncation=True)
+            print('tokenize inputs over')
+            exit(0)
         # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
         # padding in the loss.
         if padding == "max_length" and args.ignore_pad_token_for_loss:
