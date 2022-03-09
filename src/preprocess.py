27ae9cf0ea206e6822455ff5eef864f5b370153c
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-03-09 13:55:25 -0500

    coding for negative

--- src/preprocess.py
+++ src/preprocess.py
@@ -63,6 +63,7 @@ def load_instruction_from_json_data(fil):
     NEG = ''
     for id, ex in enumerate(data["Examples"]["Negative Examples"]):
         NEG+='[NEG'+str(id+1)+'] '
+        print('ex:>>', ex)
         print('ex input:>>', ex['input'].encode('utf-8').decode('utf-8'))
         print('ex output:>>', ex['output'])
         neg_ex_tuple_list.append((ex['input'].replace('Question:', '[Question]').replace('\n', ' ').strip(), ex['output'].strip()))
