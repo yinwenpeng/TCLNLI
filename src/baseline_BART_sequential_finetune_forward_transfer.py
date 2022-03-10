5f8ae629b465e352cff45a2fff75d854534e0e2a
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-03-09 21:45:38 -0500

    final forward baseline bart

--- src/baseline_BART_sequential_finetune_forward_transfer.py
+++ src/baseline_BART_sequential_finetune_forward_transfer.py
@@ -551,8 +551,8 @@ if __name__ == "__main__":
 
 '''
 
-"finetune on instructions"
-CUDA_VISIBLE_DEVICES=1 accelerate launch baseline_BART_sequential_finetune_forward_transfer.py --model_name_or_path /home/tup51337/tmp/tmp --max_source_length 1024 --output_dir /home/tup51337/tmp/tmp3 --per_device_train_batch_size=2 --per_device_eval_batch_size=24 --num_train_epochs 2 --learning_rate 2e-5 > log.finetune.forward.transfer.txt 2>&1
+"sequential finetune on instructions"
+CUDA_VISIBLE_DEVICES=1 accelerate launch baseline_BART_sequential_finetune_forward_transfer.py --model_name_or_path /home/tup51337/tmp/pretrained_BART_on_paper_tasks --max_source_length 1024 --output_dir /home/tup51337/tmp/tmp3 --per_device_train_batch_size=2 --per_device_eval_batch_size=24 --num_train_epochs 2 --learning_rate 2e-5 > log.finetune.forward.3.9.22.txt 2>&1
 
 
 
