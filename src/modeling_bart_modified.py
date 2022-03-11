a85a83bf62ae8982ebf8adbed776c4c6537c107a
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-03-11 12:45:37 -0500

    fix bug

--- src/modeling_bart_modified.py
+++ src/modeling_bart_modified.py
@@ -1321,12 +1321,14 @@ class BartForConditionalGeneration(BartPretrainedModel):
             loss_fct = CrossEntropyLoss()
             masked_lm_loss = loss_fct(lm_logits.view(-1, self.config.vocab_size), labels.view(-1))
 
+
+            '''1.0-logits'''
+
+            reverse_logits = 1.0-lm_logits.view(-1, self.config.vocab_size)
             print('reverse_logits.shape:', reverse_logits.shape)
             print('labels.shape:', labels.shape)
             print('neg_labels.shape:', neg_labels.shape)
-            '''1.0-logits'''
             neg_labels=neg_labels[:,:labels.shape[1]]
-            reverse_logits = 1.0-lm_logits.view(-1, self.config.vocab_size)
 
             #view(-1) doesnt work, so use reshape
             neg_masked_lm_loss = loss_fct(reverse_logits, torch.reshape(neg_labels, (-1,)))
