3650dd3912bd8a48af246aaf4541c4cebd000242
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-03-13 17:36:13 -0400

    coding

--- src/forfun.py
+++ src/forfun.py
@@ -5,7 +5,7 @@ values=[ 69.5813,69.5813,61.4532,71.3054,65.7635,76.9704,70.3202,71.4286,72.2906
 def computer_mean_std(value_list):
     average = round(sum(value_list)/len(value_list), 2)
     res = round(statistics.pstdev(value_list),2)
-    return str(average)+'/'+str(res)
+    print( str(average)+'/'+str(res))
 
 
 if __name__ == "__main__":
