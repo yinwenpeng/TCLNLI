8d60b218b8a6d55f4dc3027171d500886b57434b
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-03-14 16:36:52 -0400

    coding

--- src/Code_on_TCLNLI_split/baseline.seq.finetune.forward.py
+++ src/Code_on_TCLNLI_split/baseline.seq.finetune.forward.py
@@ -691,7 +691,6 @@ def postprocess_text(preds, labels):
     preds = [pred.strip() for pred in preds]
     labels = [label.strip() for label in labels]
 
-    # rougeLSum expects newline after each sentence
     preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
     labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
 
