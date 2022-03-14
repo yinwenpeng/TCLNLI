3ce8e1f7619c9a64e90dc20032ac05ff3eac67b9
Wenpeng Yin <yinwenpeng@Wenpengs-MacBook-Pro.local>
2022-03-13 21:14:09 -0400

    coding

--- src/forfun.py
+++ src/forfun.py
@@ -1,6 +1,6 @@
 
 import statistics
-values=[ 69.5279,91.4393,91.3795,90.2958,89.5093,91.3589,91.3906,86.4336,91.5119]
+values=[ 91.4393,91.3795,90.2958,89.5093,91.3589,91.3906,86.4336,91.5119]
 
 def computer_mean_std(value_list):
     average = round(sum(value_list)/len(value_list), 2)
